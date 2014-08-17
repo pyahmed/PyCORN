730eb960d43fb19527de1fff7af77e5f768f5267
pyahmed <pyahmed@users.noreply.github.com>
2014-08-17 11:03:01 +0200

    Now with fractions
    v0.11
    ======
    - changed data structure (now mostly dicts)
    - made plotting independent of writing csv/txt files
    - all plots contain (if present) fractions
    - x-axis scaling is based on first and last fraction
    - plots can be saved now in all matplotlib-supported formats (default:
    pdf)

--- pycorn.py
+++ pycorn.py
@@ -3,7 +3,7 @@
 PyCORN - script to extract data from .res (results) files generated 
 by UNICORN Chromatography software supplied with ÄKTA Systems
 (c)2014 - Yasar L. Ahmed
-v0.1
+v0.11
 '''
 
 import struct
@@ -15,7 +15,7 @@ import time
 start = time.time()
 
 parser = argparse.ArgumentParser(
-    description = "Extract data from UNICORN .res files to .csv/.txt",
+    description = "Extract data from UNICORN .res files to .csv/.txt and plot them (matplotlib required)",
     epilog = "Make it so!")
 parser.add_argument("-c", "--check", 
                     help = "Perform simple check if file is supported",
@@ -35,8 +35,10 @@ parser.add_argument("-e", "--extract",
 parser.add_argument("-p", "--plot", 
                     help = 'Plot curves',
                     action = "store_true")
-parser.add_argument("-f", "--format", type = str, default = 'pdf', 
-                    help = "File format of plot files")
+parser.add_argument("-f", "--format", type = str,
+                    choices=['svg','svgz','tif','tiff','jpg','jpeg','png','ps','eps','raw','rgba','pdf','pgf'],
+                    default = 'pdf',
+                    help = "File format of plot files (default: pdf)")
 parser.add_argument("-u", "--user", 
                     help = "Show stored user name",
                     action = "store_true")
@@ -98,7 +100,7 @@ def input_check(inp):
 
 def readheader(inp):
     '''
-    Extracts all the entries in the header (starts at position 686)
+    Extracts all the entries/declarations in the header (starts at position 686)
     '''
     tmp_list = []
     with open(inp, 'rb') as fo:
@@ -106,7 +108,22 @@ def readheader(inp):
         header_end = fread.find(LogBook_id) + 342
         for i in range(686,header_end, 344):
             decl = struct.unpack("8s296s4i", fread[i:i+320])
-            x = decl[0], codecs.decode(decl[1], 'cp1250').rstrip("\x00"),decl[2],decl[3],decl[4],decl[5]
+            full_label = codecs.decode(decl[1], 'cp1252').rstrip("\x00")
+            if full_label.find(':') == -1:
+                r_name = ''
+                d_name = full_label
+            else:
+                r_name = full_label[:full_label.find(':')]
+                d_name = full_label[full_label.find('_')+1:]
+            x = dict(magic_id = decl[0],
+                     run_name = r_name,
+                     data_name = d_name,
+                     d_size = decl[2],
+                     off_next = decl[3],
+                     adresse = decl[4],
+                     off_data = decl[5],
+                     d_start = decl[4] + decl[5],
+                     d_end = decl[4] + decl[2])
             tmp_list.append(x)
     return(tmp_list)
 
@@ -117,12 +134,15 @@ def showheader(inp,full="true"):
     '''
     header = readheader(file_in)
     print((" ---- \n Header of {0}: \n").format(inp))
-    print("  ENTRY_NAME, BLOCK_SIZE, OFFSET_TO_NEXT, ADRESSE, OFFSET_TO_DATA")
+    if full == "true":
+        print("  MAGIC_ID, ENTRY_NAME, BLOCK_SIZE, OFFSET_TO_NEXT, ADRESSE, OFFSET_TO_DATA")
+    else:
+        print(" ENTRY_NAME, BLOCK_SIZE, OFFSET_TO_NEXT, ADRESSE, OFFSET_TO_DATA")
     for i in header:
         if full == "true":
-            print(i)
+            print(" ", i['magic_id'],i['data_name'],i['d_size'],i['off_next'],i['adresse'],i['off_data'])
         else:
-            print(i[1:])
+            print("",i['data_name'], i['d_size'], i['off_next'], i['adresse'], i['off_data'])
 
 
 def showuser(inp):
@@ -132,101 +152,88 @@ def showuser(inp):
     with open(inp, 'rb') as fo:
         fread = fo.read(512)
         u = struct.unpack("40s", fread[118:158])
-        dec_u = codecs.decode(u[0], 'cp1250').rstrip("\x00")
+        dec_u = codecs.decode(u[0], 'cp1252').rstrip("\x00")
         print((" ---- \n User: {0}").format(dec_u))
 
 
-def dataextractor(in_tup):
+def dataextractor(inp):
     '''
     Identify data type by comparing magic id, then run appropriate
-    function to extract data, return data as list
+    function to extract data, update orig. dict to include new data
     '''
-    inp_id = in_tup[0]
-    data_tup = in_tup[1:]
-    #skip empty blocks
-    if (in_tup[2],in_tup[3]) == (0, 0):
+    if inp['d_size'] == 0:
         pass
-    elif inp_id == CNotes_id or inp_id == Methods_id:
-        x = meta2_read(data_tup)
-        x.insert(0,inp_id)
-        return x
-    elif inp_id == Logbook_id or inp_id == Inject_id or Fractions_id == inp_id:
-        return meta1_read(data_tup)
-    elif inp_id == SensData_id:
-        return sensor_read(data_tup)
+    elif inp['magic_id'] == CNotes_id or inp['magic_id'] == Methods_id:
+        inp.update(data=meta2_read(inp))
+        return inp
+    elif inp['magic_id'] == Logbook_id or inp['magic_id'] == Inject_id or inp['magic_id'] == Fractions_id:
+        inp.update(data=meta1_read(inp))
+        return inp
+    elif inp['magic_id'] == SensData_id:
+        values,unit=sensor_read(inp)
+        inp.update(data=values, unit=unit)
+        return inp
     else:
         pass
 
 
-def meta1_read(in_tup, silent="false"):
+def meta1_read(inp, silent="false"):
     '''
     Extracts meta-data/type1, Logbook, fractions and Inject marks
     '''
     if silent == "true":
         pass
     else:
-        print((" Extracting: {0}").format(in_tup[0]))
+        print((" Extracting: {0}").format(inp['data_name']))
     finaldata = []
-    start = start = in_tup[3] + in_tup[4]
-    end = in_tup[3] + in_tup[1]
     with open(file_in, 'rb') as fo:
         fread = fo.read()
-        for i in range(start, end, 180):
+        for i in range(inp['d_start'], inp['d_end'], 180):
             dp = struct.unpack("dd158s", fread[i:i+174])
             #acc_time = dp[0] # not used atm
             acc_volume = round(dp[1]-inj_sel,4)
             label = (codecs.decode(dp[2], 'cp1250')).rstrip('\x00')
             merged_data=acc_volume,label
             finaldata.append(merged_data)
-    finaldata.insert(0, in_tup[0])
     return(finaldata)
 
 
-def meta2_read(in_tup):
+def meta2_read(inp):
     '''
-    Extracts meta-data/type2, Method used in the run
+    Extracts meta-data/type2, Method/Program used in the run
     '''
-    print((" Extracting: {0}").format(in_tup[0]))
-    start = in_tup[3] + in_tup[4]
+    print((" Extracting: {0}").format(inp['data_name']))
     with open(file_in,'rb') as fo:
-        fo.seek(start)
-        tmp_data = fo.read(in_tup[1])
+        fo.seek(inp['d_start'])
+        tmp_data = fo.read(inp['d_size'])
         size = tmp_data.rfind(b'\n') #declared block-size in header is always off
-        fo.seek(start)               #by a few bytes, hence it is redetermined here
+        fo.seek(inp['d_start'])        #by a few bytes, hence it is redetermined here
         data = fo.read(size)
-    output = [in_tup[0],data]
-    return(output)
+    return(data)
 
 
-def sensor_read(in_tup):
+def sensor_read(inp):
     '''
     extracts sensor/run-data and applies correct division
     '''
     tmp_list0 = []
-    # set positions
-    start = in_tup[3] + in_tup[4]
-    end = in_tup[3] + in_tup[1]
-    # get the labels
-    sep0 = in_tup[0].find(":")
-    sep1 = in_tup[0].find("_")
-    #run_label = (in_tup[0])[:sep0] # not used atm
-    sensor_label = (in_tup[0])[sep1+1:]
-    # set the dividor
-    if "UV" in sensor_label or "Cond" == sensor_label or "Flow" == sensor_label:
+    if "UV" in inp['data_name'] or "Cond" == inp['data_name'] or "Flow" == inp['data_name']:
         sensor_div = 1000.0
-    elif "Pressure" in sensor_label:
+    elif "Pressure" in inp['data_name']:
         sensor_div = 100.0
     else:
         sensor_div = 10.0
-    print((" Extracting: {0}").format(in_tup[0]))
+    print((" Extracting: {0}").format(inp['data_name']))
     with open(file_in, 'rb') as fo:
         fread = fo.read()
-        for i in range(start, end, 8):
+        for i in range(inp['adresse']+207, inp['adresse']+222, 15):
+            s_unit = struct.unpack("15s", fread[i:i+15])
+            s_unit_dec = (codecs.decode(s_unit[0], 'cp1252')).rstrip('\x00')
+        for i in range(inp['d_start'], inp['d_end'], 8):
             sread = struct.unpack("ii", fread[i:i+8])
             data=round((sread[0]/100.0)-inj_sel,4),sread[1]/sensor_div
             tmp_list0.append(data)
-        tmp_list0.insert(0, in_tup[0])
-    return(tmp_list0)
+    return(tmp_list0,s_unit_dec)
 
 
 def inject_det(inp,show="false"):
@@ -236,8 +243,8 @@ def inject_det(inp,show="false"):
     injection_points = []
     header = readheader(inp)
     for i in header:
-        if i[0] == Inject_id:
-            injection = meta1_read(i[1:],silent="true")[1][0]
+        if i['magic_id'] == Inject_id:
+            injection = meta1_read(i,silent="true")[0][0]
             injection_points.append(injection)
             if injection != 0.0:
                 injection_points.insert(0,0.0)
@@ -269,82 +276,118 @@ def store_in_list(inp):
             data_storage.append(x)
 
 
-def writer(in_list):
+def writer(inp):
     '''
     general writer, picks the correct writer function based on data type
     '''
-    if in_list[0] == Methods_id or in_list[0] == CNotes_id:
-        meta_writer(in_list[1:])
+    if inp['magic_id'] == Methods_id or inp['magic_id'] == CNotes_id:
+        meta_writer(inp)
     else:
-        data_writer(in_list)
+        data_writer(inp)
 
 
-def meta_writer(in_list):
+def meta_writer(inp):
     '''
     writes meta-data to txt-files
     '''
-    ext = "_"+in_list[0]+".txt"
-    inp_data = codecs.decode(in_list[1], 'cp1250')#.replace("\r","")
-    content = inp_data.encode('utf-8')
+    ext = "_"+inp['data_name']+".txt"
+    unicode_data = codecs.decode(inp['data'], 'cp1250')
+    content = unicode_data.encode('utf-8')
     with open(file_base+ext,'wb') as fout:
-        print(" Writing %s" %in_list[0])
-        fout.write(content)
+        print((" Writing {0}").format(inp['data_name']))
+        fout.write(inp['data'])
 
              
-def data_writer(in_list):
+def data_writer(inp):
     '''
     writes sensor/run-data to csv-files
     '''
-    sep = in_list[0].find(":")+1
-    label = in_list[0][sep:]
-    run_name = in_list[0][:sep-1]
-    fname = file_base+"_"+run_name+"_"+label+".csv"
-    if "Logbook" in label:
+    fname = file_base+"_"+inp['run_name']+"_"+inp['data_name']+".csv"
+    if "Logbook" in inp['data_name']:
         pass
     else:
         with open(fname, 'wb') as fout:
-            print((" Writing {0}").format(label))
-            for i in in_list[1:]:
+            print((" Writing {0}").format(inp['data_name']))
+            for i in inp['data']:
                 dp = (str(i[0]) + "," + str(i[1]) + str("\n")).encode('utf-8')
                 fout.write(dp)
 
+def mapper(min_val,max_val,perc):
+    '''
+    calculate relative position in delta min/max
+    '''
+    x = abs(max_val-min_val)*perc
+    if min_val < 0:
+        return(x - abs(min_val))
+    else:
+        return(x + min_val)
 
-def plotter(in_list):
-    sep = in_list[0].find(":")+1
-    label = in_list[0][sep:]
-    run_name = in_list[0][:sep-1]
-    f_list = in_list[1:] #filter the list
-    x_val=[x[0] for x in f_list]
-    y_val=[x[1] for x in f_list]
-    plot_x_min = in_list[1][0]
-    plot_x_max = in_list[-1][0]
+
+def expander(min_val,max_val,perc):
+    '''
+    expand -/+ direction of two values by a percentage of their delta
+    '''
+    delta = abs(max_val-min_val)
+    x = delta*perc 
+    return(min_val - x, max_val + x)
+
+def plotter(inp,fractions):
+    x_val = [x[0] for x in inp['data']]
+    y_val = [x[1] for x in inp['data']]
+    if fractions == None:
+        plot_x_min = x_val[0]
+        plot_x_max = x_val[-1]
+    else:
+        plot_x_min = fractions[0][0]
+        plot_x_max = fractions[-1][0]
+    plot_y_min = expander(min(y_val),max(y_val),0.04)[0]
+    plot_y_max = expander(min(y_val),max(y_val),0.04)[1]
+    ax = plt.gca()
     if type(y_val[0]) == float or type(y_val[0]) == int:
-        print(" Plotting " + label)
         plt.xlim(xmin = plot_x_min, xmax = plot_x_max)
-        plt.title(file_in + " - " + run_name + " - " + label)
+        plt.ylim(ymin = plot_y_min, ymax = plot_y_max)
+        frac_y_pos = mapper(ax.get_ylim()[0],ax.get_ylim()[1],0.02)
+        plt.title(file_in + " - " + inp['run_name'] + " - " + inp['data_name'], size=12)
+        plt.ylabel(inp['unit'])
         plt.xlabel('ml')
-        plt.grid(which='major', axis='both')
-        plt.plot(x_val, y_val,linewidth=1.5, alpha=0.85,color='b')
+        ax.spines['right'].set_color('none')
+        ax.spines['top'].set_color('none')
+        plt.tick_params(axis='x',direction='out',top='off')
+        plt.tick_params(axis='y',direction='out',right='off')
+        plt.plot(x_val, y_val,linewidth=1.5, color='b')
+        if fractions != None:
+            frac_x = [x[0] for x in fractions]
+            frac_tmp = frac_x[1:]
+            frac_tmp.append(0)
+            frac_delta = [abs(a - b) for a,b in zip(frac_tmp,frac_x)]
+            for i in fractions:
+                plt.axvline(x=i[0], ymin=0.065, ymax=0.0,color='r', linewidth=0.85)
+                plt.annotate(i[1], xy=(i[0]+frac_delta[fractions.index(i)]*0.1,frac_y_pos), verticalalignment='bottom', size=8, rotation=90)
+        print(" Plotting " + inp['data_name'])
         ext = "." + (args.format)[-3:]
-        fname = file_base + "_Plot_" + run_name + "_" + label + ext
-        plt.savefig(fname, dpi=300)
+        file_name = file_base + "_Plot_" + inp['run_name'] + "_" + inp['data_name'] + ext
+        plt.savefig(file_name, papertype='a4',dpi=300)
         plt.clf()
 
 
 def endscript():
-    print("stopping")
+    print(" Plotting canceled")
     exit
 
 #--- run time---#
 
 if args.user:
     showuser(file_in)
+    
 if args.check:
     input_check(file_in)
+    
 if args.info:
     showheader(file_in,full="false")
+    
 if args.points:
     inject_det(file_in,show="true")
+    
 if args.plot:
     if input_check(file_in) == True:
         try:
@@ -356,10 +399,18 @@ if args.plot:
             print("\n Matplotlib found - WARNING - PLOTTING IS EXPERIMENTAL\n")
             data_storage = []
             store_in_list(file_in)
+            magic_ids = [x['magic_id'] for x in data_storage]
+            if Fractions_id in magic_ids:
+                fraction_idx = magic_ids.index(Fractions_id)
+                fractions = data_storage[fraction_idx]['data']
+            else:
+                fractions = None
             for i in data_storage:
-                plotter(i)
+                if i['magic_id'] == SensData_id:
+                    plotter(i,fractions)
     else:
         pass
+
 if args.extract:
     if input_check(file_in) == True:
         data_storage = []
