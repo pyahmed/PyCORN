b05b8ae8823193973aafdf31bc282e3230636d00
pyahmed <pyahmed@users.noreply.github.com>
2016-01-19 20:48:33 +0100

    version bump

--- pycorn/pycorn.py
+++ pycorn/pycorn.py
@@ -2,8 +2,8 @@
 '''
 PyCORN - script to extract data from .res (results) files generated
 by UNICORN Chromatography software supplied with ÄKTA Systems
-(c)2014-2015 - Yasar L. Ahmed
-v0.14
+(c)2014-2016 - Yasar L. Ahmed
+v0.17
 '''
 
 from __future__ import print_function
