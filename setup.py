ac5ada58ba42028c42e2794620d04f86cfdfecd6
pyahmed <pyahmed@users.noreply.github.com>
2016-01-19 20:58:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='pycorn',
-    version='0.14',
+    version='0.17',
     author='Yasar L. Ahmed',
     packages=['pycorn'],
     extras_require = {'plotting':  ["matplotlib"], 'xlsx-output': ['xlsxwriter']},
